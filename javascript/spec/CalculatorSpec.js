cbb297dfe26dc930ff28ec326f802bbe47bb8b71
aheld <aarondheld@gmail.com>
2013-02-14 12:31:12 -0500

    day 1.3

--- javascript/spec/CalculatorSpec.js
+++ javascript/spec/CalculatorSpec.js
@@ -6,7 +6,7 @@ beforeEach(function() {
   });
 });
 
-describe("Add", function() {
+describe("Add - Single digit cases", function() {
   it("should return 0 for an empty string input", function() {
     expect(Add("")).toEqual(0);
     });
@@ -15,12 +15,47 @@ describe("Add", function() {
         expect(Add(x[0])).toEqual(x[1]);
       });
     });
-  it("should return 3 for an string input of '1,2'", function() {
-    var result = Add("1,2");
-    expect(result).toEqual(3);
+
+ 
+
+});
+
+describe("Add - Multiple digit cases", function() {
+    it("should return 3 for an string input of '1\\n2'", function() {
+    expect(Add("1\n2")).toEqual(3);
     });
-  it("should return Error: fred for an string input of '1,2,3,4'", function() {
-    expect(function(){Add("1,2,3,4");}).toThrow(Error("fred"));
+    it("should return 3 for an string input of '1,2'", function() {
+    expect(Add("1,2")).toEqual(3);
+    });
+  it("should return 5 for an string input of '1,2,2'", function() {
+    expect(Add("1\n2\n2")).toEqual(5);
+    });
+ it("should return 5 for an string input of '1,2\\n2'", function() {
+    expect(Add("1,2\n2")).toEqual(5);
     });
 
+});
+
+describe("Delimeted parsing", function() {
+    it("should return the same string passed in when it does not start with //", function() {
+      var inp = "aksdjkasjfklasf";
+    expect(returnDelimAndNumbers(inp).numbers).toEqual(inp);
+    });
+    it("should return numbers of '1;2' and delimeter of '[\\n,;]' for an input of '//;\\n1;2'", function() {
+      var inp = "//;\n1;2";
+    expect(returnDelimAndNumbers(inp).numbers).toEqual('1;2');
+    expect(returnDelimAndNumbers(inp).delimeter).toEqual('[\n,;]');
+    });
+});
+
+describe("Add - Variable Delimeter", function() {
+    it("should return numbers of '6' for an input of '//;\\n1;2;3'", function() {
+      var inp = "//;\n1;2;3";
+    expect(Add(inp)).toEqual(6);
+    });
+    it("should return the correct answer for mixed delims such as //;\n1;2\n3", function() {
+    expect(Add('//;\n1;2\n3')).toEqual(6);
+    expect(Add('//;\n1;2\n3')).toEqual(6);
+    expect(Add('//;\n1;2,3')).toEqual(6);
+    });
 });
--- javascript/src/Calculator.js
+++ javascript/src/Calculator.js
@@ -1,9 +1,24 @@
-function Add(numbers) {
-	var tmp = numbers.split(',');
+function AddSimple(numbers,delimeter) {
+	var tmp = numbers.split(new RegExp(delimeter));
 	var result =  0;
-	if (tmp.length > 2) throw Error("fred");
 	tmp.forEach(function(x){
 		result += Number(x);
 	});
 	return result;
+}
+
+function Add(numbers){
+	var tmp = returnDelimAndNumbers(numbers);
+	return AddSimple(tmp.numbers,tmp.delimeter);
+}
+
+function returnDelimAndNumbers(numbers){
+	var delimeter = ["\n",","];
+	if (numbers.indexOf("//") === 0){
+		delimeter.push(numbers.slice(2,3));
+		numbers = numbers.slice(4);
+	}
+	delimeter = "[" + delimeter.join("") + "]";
+	return {'numbers':numbers,'delimeter':delimeter};
+
 }
