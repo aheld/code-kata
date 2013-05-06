d2db49198f99f9f63fe3e3de81100e73fd72e7a0
aheld <aarondheld@gmail.com>
2013-05-06 10:29:51 -0400

    no nose required

--- python/README.md
+++ python/README.md
@@ -4,3 +4,6 @@ Bootstrap for Python
 - Create a Python2.7 virtualenv
 - install nose
     - pip install -r requirements.txt
+
+or 
+- python calculator_test.py
--- python/calculator_test.py
+++ python/calculator_test.py
@@ -1,5 +1,9 @@
 from calculator import *
+import unittest
 
-class Test_Calculator:
+class Test_Calculator(unittest.TestCase):
     def test_add_1(self):
         assert(add("")==0)
+
+if __name__ == '__main__':
+    unittest.main()
